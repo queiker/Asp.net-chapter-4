5d45b70baa24f748ac46177497a7e6dd60f4b184
Grzegorz Płonka <tradecomp.pl@gmail.com>
2021-06-14 17:30:46 +0200

    done at page 65

--- MyProject/Startup.cs
+++ MyProject/Startup.cs
@@ -35,6 +35,7 @@ namespace MyProject
             }
             else
             {
+                app.UseStaticFiles();
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
